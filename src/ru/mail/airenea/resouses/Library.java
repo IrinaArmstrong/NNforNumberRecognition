fd7d7c66afad3bcf2e0e01603af2573e03096995
IrinaArmstrong <a.irene.a@mail.ru>
2018-07-24 21:45:24 +0300

    All for last commit again

--- src/ru/mail/airenea/resouses/Library.java
+++ src/ru/mail/airenea/resouses/Library.java
@@ -1,22 +1,37 @@
 package ru.mail.airenea.resouses;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Library {
 
     private static String dirPath = "C:\\Users\\Ирина\\Documents\\Программирование\\Нейронные сети\\Выборки для НС\\Numbers";
 
     private static File dir = new File(dirPath);
-    private static File file;
+    private static int num;
 
-    private static BufferedImage image;
+    // Returns set of training pairs of task and answer
+    public ArrayList <TaskAnswerPair> getSet(int num) {
+        ArrayList <TaskAnswerPair> set = new ArrayList<TaskAnswerPair>();
+        for (int i = 0; i < num; i++)  {
+            set.add(new TaskAnswerPair(getPixels(), num));
+        }
+        return set;
+    }
 
-    // Get matrix of pixels.
-    public static byte[][] get()  {
+    // Returns one training pair of task and answer
+    public  TaskAnswerPair getOne() {
+        return new TaskAnswerPair(getPixels(), num);
+    }
 
+    // Get matrix of pixels
+    public static byte[][] getPixels()  {
+
+        BufferedImage image = getImageFromFile();
         int width = image.getWidth();
         int height = image.getHeight();
         byte[][] pixels = new byte[height][width];
@@ -30,21 +45,52 @@ public class Library {
     }
 
 
-    //Get certain image from file system.
-    private static void getImageFromFile()  {
+    //Get certain image from file system
+    private static BufferedImage getImageFromFile()  {
 
         String imgURL = "num" + generateRandNumber() + ".jpg";
-        file = new File(dir, imgURL);
+        File file = new File(dir, imgURL);
         try {
-            image = ImageIO.read(file);
+            BufferedImage image = ImageIO.read(file);
+            return image;
         } catch (IOException e) {
             System.err.println("Error in reading from file: " + e.getMessage());
+            return null;
         }
     }
 
-    // Generating random number.
+    // Generating random number
     private static int generateRandNumber()  {
-        int num = 0;
-        return  num;
+        int number = (int) Math.random() * 10;
+        num = number;
+        return  number;
+    }
+
+    // Class, to store a pair: task and right answer
+    class TaskAnswerPair {
+
+        private byte[][] task;
+        private int number;
+
+        public TaskAnswerPair(byte[][] task, int number) {
+            this.task = task;
+            this.number = number;
+        }
+
+        public byte[][] getTask() {
+            return task;
+        }
+
+        public void setTask(byte[][] task) {
+            this.task = task;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
     }
 }
