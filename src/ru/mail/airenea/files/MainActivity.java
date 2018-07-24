56901181c833e241283e472a256344485960a677
IrinaArmstrong <a.irene.a@mail.ru>
2018-07-24 21:43:55 +0300

    Methods to get one pair and a set created, also class of TaskAnswerPair

--- src/ru/mail/airenea/files/MainActivity.java
+++ src/ru/mail/airenea/files/MainActivity.java
@@ -1,4 +1,11 @@
 package ru.mail.airenea.files;
 
+import java.util.ArrayList;
+import ru.mail.airenea.resouses.Library.*;
+
 public class MainActivity {
+
+    public static void main(String[] args) {
+
+    }
 }
